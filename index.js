b27ab644e0989c818b6ada5e59ae94dc8c8f749e
Andy Tang <bossbadi123@gmail.com>
2023-02-03 00:55:18 -0500

    make copy of game files outside public dir

---
+++ index.js
@@ -0,0 +1,28 @@
+// this sets up the server
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const port = process.env.PORT || 80;
+const path = require("path");
+
+// this adds the required headers to the response
+app.use(function (req, res, next) {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
+app.use(express.static("public"));
+
+// this starts the server
+server.listen(port, function () {
+  console.log("Listening on port:", port);
+});
+
+//index.js
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+
+// index.js
+module.exports = app;
