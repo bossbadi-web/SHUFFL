feead2395591580ab0a181d33cf464e93cbc3707
Andy Tang <bossbadi123@gmail.com>
2023-02-03 01:32:33 -0500

    update index

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(express.static("public"));
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['html','css','js', 'png', 'pck', 'wasm'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+app.use(express.static("public", options));
 
 // this starts the server
 server.listen(port, function () {
