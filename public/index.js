4315847198289a16496690308dcc37208ba993c2
Andy Tang <bossbadi123@gmail.com>
2023-02-03 00:50:39 -0500

    update index.js

--- public/index.js
+++ public/index.js
@@ -12,7 +12,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-// this hosts all the files in the 'public' folder (you should put all the files in your original HTML, CSS, and JS Repl in a folder named "public", you can change the name of the folder hosted by changing the name below)
 app.use(express.static("public"));
 
 // this starts the server
@@ -22,7 +21,7 @@ server.listen(port, function () {
 
 //index.js
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+  res.sendFile("index.html");
 });
 
 // index.js
